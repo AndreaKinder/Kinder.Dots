b51d21659977b7106a4a3c01c6cafb8e98a17b2d
Andrea <144063021+AndreaKinder@users.noreply.github.com>
2024-10-07 13:57:57 +0200

    add automation instalation scripts

--- aur-apps.sh
+++ aur-apps.sh
@@ -3,4 +3,4 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-yay -S warpinator cpod greenclip obsidian bitwarden qtile piper rofi picom ttf-iosevka-nerd
+yay -S warpinator cpod greenclip obsidian bitwarden-rofi-git qtile piper picom ttf-iosevka-nerd fslint fslint-gui libinput-gestures lightdm-webkit-theme-aether obsidian piper playerctld-systemd-unit verascrypt yay yay-debug cython2 cython2-debug libglade libglade-debug libinput-gestures lightdm-webkit-theme-aether playerctld-systemd-unit pygtk pygtk-debug python2 python2-cairo python2-cairo-debug python2-gobject2 python2-numpy python2-numpy-debug 
